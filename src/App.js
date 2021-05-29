6f5bc1766dbe3733697c7734a82059e35ae4d76b
Tejas Kumthekar <tejas.kumthekar26@gmail.com>
2021-05-28 18:45:36 -0700

    Add image upload to nft.storage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import { NFTStorage } from 'nft.storage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiKey = process.env.REACT_APP_NFT_STORAGE_API_KEY;
+const client = new NFTStorage({ token: apiKey });
+
+class App extends Component {
+  state = {
+    selectedFile: null,
+    cid: null
+  };
+
+  onFileChange = event => {
+    this.setState({ selectedFile: event.target.files[0] });
+  };
+
+  onFileUpload = async() => {
+    const cid = await client.storeBlob(this.state.selectedFile);
+
+    this.setState({ cid });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Welcome to Open PID</h1>
+        </header>
+
+        <hr />
+        <h3>Choose a file</h3>
+
+        <input type="file" onChange={this.onFileChange} />
+        <button onClick={this.onFileUpload}>
+          Upload!
+        </button>
+
+        {this.state.cid}
+
+        <hr />
+      </div>
+    );
+  }
 }
 
 export default App;
